fc888f10d26e01d70487a905e32785e984e57db6
migue <miguel.jesus.t0123771@edu.atec.pt>
2023-10-05 15:52:09 +0100

    Changes by bruno, com bugs

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/models' ,[BrandController::class, 'index'])->name('models.index');
 Route::get('/', function () {
     return 'ola abelhasSSS';
 });
