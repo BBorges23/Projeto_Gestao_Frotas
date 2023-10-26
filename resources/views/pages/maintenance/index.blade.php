53c5e9c5bfdaa4093056949e546e901e45a53ef2
BrunoOrike <bborges31@gmail.com>
2023-10-26 18:02:11 +0100

    Changes by migue

--- resources/views/pages/maintenance/index.blade.php
+++ resources/views/pages/maintenance/index.blade.php
@@ -27,35 +27,6 @@
                 @endcomponent
             </div>
         @endforeach
-
-        @role('driver')
-        @if ($maintenance_mot)
-            @foreach($maintenance_mot as $maintenance)
-                <!-- Se a variável $travels_mot não for nula, faça o loop -->
-                <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Vehicle ID</th>
-                        <th>Driver Name</th>
-                        <th>Created AT</th>
-                        <th>Updated AT</th>
-                    </tr>
-                    <tr>
-                        <td>{{$maintenance->id}}</td>
-                        <td>{{$maintenance->vehicle_id}}</td>
-                        <td>{{ session('driver')->name }}</td> <!-- Acessa o nome do motorista na sessão -->
-                        <td>{{$maintenance->date_entry}}</td>
-                        <td>{{$maintenance->motive}}</td>
-                        <td>{{$maintenance->date_exit}}</td>
-                    </tr>
-                </table>
-            @endforeach
-        @else
-            <!-- Caso a variável $travels_mot seja nula -->
-            <p>Nenhuma viagem encontrada para este motorista.</p>
-        @endif
-        @endrole
-
     </div>
     <a class="btn btn-success" href="{{ route('admin.maintenances.create',$maintenance->id) }}">Criar</a><br />
 
