a57cd8cf3b5907e2ad6958d2176215e4763f0e6a
migue <miguel.jesus.t0123771@edu.atec.pt>
2023-10-31 18:22:18 +0000

    Changes by Rui Gameiro

--- resources/views/template/components/head.blade.php
+++ resources/views/template/components/head.blade.php
@@ -18,4 +18,7 @@
             crossorigin="anonymous">
     </script>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </head>
