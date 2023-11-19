eadf91f72c7999c661ea2886d4826608f9a063ea
BrunoOrike <bborges31@gmail.com>
2023-11-19 18:02:40 +0000

    Changes by Ruben Soares

--- database/seeders/MaintenanceSeeder.php
+++ database/seeders/MaintenanceSeeder.php
@@ -24,7 +24,7 @@ class MaintenanceSeeder extends Seeder
         //Maintenance::factory(10)->create();
         DB::table('maintenances')->insert([
             'vehicle_id' => '1',
-            'motive' => 'Óleo',
+            'motive' => 'Mudança de Óleo',
             'date_entry' => '2023-10-14',
             'date_exit'=> '2023-10-16',
             'is_active' => '1',
@@ -35,7 +35,7 @@ class MaintenanceSeeder extends Seeder
 
         DB::table('maintenances')->insert([
             'vehicle_id' => '2',
-            'motive' => 'Pneu',
+            'motive' => 'Pneu Furado',
             'date_entry' => '2023-10-12',
             'date_exit'=> '2023-10-17',
             'is_active' => '1',
@@ -46,7 +46,7 @@ class MaintenanceSeeder extends Seeder
 
         DB::table('maintenances')->insert([
             'vehicle_id' => '3',
-            'motive' => 'Motor',
+            'motive' => 'Cabeça do Motor Queimada',
             'date_entry' => '2023-10-14',
             'date_exit'=> '2023-10-20',
             'is_active' => '1',
@@ -123,7 +123,7 @@ class MaintenanceSeeder extends Seeder
 
         DB::table('maintenances')->insert([
             'vehicle_id' => '8',
-            'motive' => 'Componente Electrico',
+            'motive' => 'Revisão Anual',
             'date_entry' => '2023-10-11',
             'date_exit'=> '2023-10-16',
             'is_active' => '1',
@@ -134,9 +134,108 @@ class MaintenanceSeeder extends Seeder
 
         DB::table('maintenances')->insert([
             'vehicle_id' => '8',
-            'motive' => 'Componente Electrico',
+            'motive' => 'Troca de Bateria',
             'date_entry' => '2023-10-11',
             'date_exit'=> '2023-10-16',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '9',
+            'motive' => 'Pneu Furado',
+            'date_entry' => '2023-10-11',
+            'date_exit'=> '2023-10-16',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '11',
+            'motive' => 'Mudança de Oleo',
+            'date_entry' => '2023-11-19',
+            'date_exit'=> '2023-11-20',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '13',
+            'motive' => 'Mudança de Filtros',
+            'date_entry' => '2023-11-20',
+            'date_exit'=> '2023-11-21',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '15',
+            'motive' => 'Vidro Partido',
+            'date_entry' => '2023-11-25',
+            'date_exit'=> '2023-11-26',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '20',
+            'motive' => 'Injetores',
+            'date_entry' => '2023-06-20',
+            'date_exit'=> '2023-06-21',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '21',
+            'motive' => 'Avaria no Motor de Arranque',
+            'date_entry' => '2023-03-20',
+            'date_exit'=> '2023-03-25',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '23',
+            'motive' => 'Falha no Alternador',
+            'date_entry' => '2023-04-01',
+            'date_exit'=> '2023-04-03',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '25',
+            'motive' => 'Direcção Desalinhada',
+            'date_entry' => '2023-09-11',
+            'date_exit'=> '2023-09-11',
+            'is_active' => '1',
+            'state' => 'PROCESSANDO',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('maintenances')->insert([
+            'vehicle_id' => '30',
+            'motive' => 'Lâmpada Fundida',
+            'date_entry' => '2023-07-13',
+            'date_exit'=> '2023-07-13',
             'is_active' => '1',
             'state' => 'PROCESSANDO',
             'created_at'=>now(),
--- database/seeders/TravelSeeder.php
+++ database/seeders/TravelSeeder.php
@@ -32,7 +32,7 @@ class TravelSeeder extends Seeder
         DB::table('travels')->insert([
             'vehicle_id' => '2',
             'driver_id' => '2',
-            'coords_origem' => 'Palmela',
+            'coords_origem' => 'Almada',
             'coords_destino'=> 'Lisboa',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
@@ -44,8 +44,8 @@ class TravelSeeder extends Seeder
         DB::table('travels')->insert([
             'vehicle_id' => '3',
             'driver_id' => '3',
-            'coords_origem' => 'Palmela',
-            'coords_destino'=> 'Lisboa',
+            'coords_origem' => 'Seixal',
+            'coords_destino'=> 'Nazaré',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-01-02',
@@ -56,8 +56,8 @@ class TravelSeeder extends Seeder
         DB::table('travels')->insert([
             'vehicle_id' => '4',
             'driver_id' => '4',
-            'coords_origem' => 'Palmela',
-            'coords_destino'=> 'Lisboa',
+            'coords_origem' => 'Barreiro',
+            'coords_destino'=> 'Pinhal Novo',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-02-01',
@@ -68,8 +68,8 @@ class TravelSeeder extends Seeder
         DB::table('travels')->insert([
             'vehicle_id' => '5',
             'driver_id' => '5',
-            'coords_origem' => 'Berlin',
-            'coords_destino'=> 'Lisboa',
+            'coords_origem' => 'Évora',
+            'coords_destino'=> 'Monchique',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-03-01',
@@ -81,8 +81,8 @@ class TravelSeeder extends Seeder
         DB::table('travels')->insert([
             'vehicle_id' => '8',
             'driver_id' => '8',
-            'coords_origem' => 'Lisboa',
-            'coords_destino'=> 'Madrid',
+            'coords_origem' => 'Beja',
+            'coords_destino'=> 'Viana do Castelo',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-08-01',
@@ -95,7 +95,7 @@ class TravelSeeder extends Seeder
             'vehicle_id' => '8',
             'driver_id' => '8',
             'coords_origem' => 'Porto',
-            'coords_destino'=> 'Viana',
+            'coords_destino'=> 'Braga',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-10-11',
@@ -108,7 +108,7 @@ class TravelSeeder extends Seeder
             'vehicle_id' => '8',
             'driver_id' => '8',
             'coords_origem' => 'Sevilha',
-            'coords_destino'=> 'Moita',
+            'coords_destino'=> 'Faro',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-12-15',
@@ -120,12 +120,168 @@ class TravelSeeder extends Seeder
         DB::table('travels')->insert([
             'vehicle_id' => '8',
             'driver_id' => '8',
-            'coords_origem' => 'Cascais',
+            'coords_origem' => 'Lisboa',
             'coords_destino'=> 'Paris',
             'is_traveling' => '1',
             'state' => 'PROCESSANDO',
             'date_start'=> '2023-04-07',
             'date_end'=> '2023-04-23',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '9',
+            'driver_id' => '9',
+            'coords_origem' => 'Cascais',
+            'coords_destino'=> 'Berlim',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-19',
+            'date_end'=> '2023-11-25',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '10',
+            'driver_id' => '10',
+            'coords_origem' => 'Barcelona',
+            'coords_destino'=> 'Paris',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-20',
+            'date_end'=> '2023-11-21',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '11',
+            'driver_id' => '11',
+            'coords_origem' => 'Coimbra',
+            'coords_destino'=> 'Toulouse',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-25',
+            'date_end'=> '2023-11-25',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '12',
+            'driver_id' => '12',
+            'coords_origem' => 'Saragoça',
+            'coords_destino'=> 'Albufeira',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-26',
+            'date_end'=> '2023-11-30',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '13',
+            'driver_id' => '13',
+            'coords_origem' => 'Corunha',
+            'coords_destino'=> 'Caldas da Rainha',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-28',
+            'date_end'=> '2023-11-29',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '14',
+            'driver_id' => '14',
+            'coords_origem' => 'Vila Real',
+            'coords_destino'=> 'Montpellier',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-29',
+            'date_end'=> '2023-11-30',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+        DB::table('travels')->insert([
+            'vehicle_id' => '15',
+            'driver_id' => '15',
+            'coords_origem' => 'Pamplona',
+            'coords_destino'=> 'Viseu',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-29',
+            'date_end'=> '2023-12-05',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '16',
+            'driver_id' => '16',
+            'coords_origem' => 'Lisboa',
+            'coords_destino'=> 'Porto',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-11-19',
+            'date_end'=> '2023-11-20',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '17',
+            'driver_id' => '17',
+            'coords_origem' => 'Porto',
+            'coords_destino'=> 'Lisboa',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-01-11',
+            'date_end'=> '2023-01-16',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '18',
+            'driver_id' => '18',
+            'coords_origem' => 'Valladolid',
+            'coords_destino'=> 'Marselha',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-03-07',
+            'date_end'=> '2023-04-23',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '19',
+            'driver_id' => '19',
+            'coords_origem' => 'Valência',
+            'coords_destino'=> 'Genebra',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-08-07',
+            'date_end'=> '2023-08-20',
+            'created_at'=>now(),
+            'updated_at'=>now()
+        ]);
+
+        DB::table('travels')->insert([
+            'vehicle_id' => '20',
+            'driver_id' => '20',
+            'coords_origem' => 'Bruxelas',
+            'coords_destino'=> 'Londres',
+            'is_traveling' => '1',
+            'state' => 'PROCESSANDO',
+            'date_start'=> '2023-02-20',
+            'date_end'=> '2023-02-25',
             'created_at'=>now(),
             'updated_at'=>now()
         ]);
